321ad6a2fe2082fdcec89c60c53ae7bc06a70c7e
ytbryan <ytbryan@gmail.com>
2016-10-06 21:02:07 +0800

    added example.rb

--- example.rb
+++ example.rb
@@ -1,15 +1,23 @@
+require 'csv'
 require 'macvendors'
 
-input = ""
-output = ""
+output = "./output.csv"
+mac_path = "./input.csv" #this is the file with mac address
 
-#take in the csv
-data = CSV.read(input)
-mac_addresses = data["mac"]
-puts "This csv contains no mac address column named 'mac'" if mac_addresses.count == 0
+##################################
+
+mac_file = CSV.read(mac_path,:headers=> true, :encoding => "ISO8859-1:utf-8")
+
 MacVendors.setup
 
-#output
-mac_addresses.times.each_with_index do |index, row|
-csv << MacVendors.find(row)
+CSV.open(output, "a+") do |csv|
+  mac_file.each_with_index do |row, index|
+    a_mac_address = row[4] #4 refers to the fifth column of the csv where mac address is
+    answer = MacVendors.find(a_mac_address)
+    array = []
+    row.each_with_index do |_each_value,index|
+      array.push(row[index])
+    end
+  	csv << array.push(answer)
+  end
 end
